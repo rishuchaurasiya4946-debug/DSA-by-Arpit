e64efaf8f0324c7902db89bdcf5362911f0a9c72
Arpitt_22 <rishuchaurasiya4946@gmail.com>
2026-08-14 01:47:26 +0530

    Time: 0 ms (100%), Space: 67.5 MB (23.85%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> freq(nums.size(),0);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+            if(freq[nums[i]]>1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
